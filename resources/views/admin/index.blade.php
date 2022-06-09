3faf73505c8730137056605db7b9b7de083235b9
sneyker408 <sneyker408@hotmail.com>
2022-06-09 07:32:14 -0600

    fata modificar compras

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -11,44 +11,52 @@
 <div class="card" style="width: 18rem;">
   <img src="https://www.ecommercenews.pe/wp-content/uploads/2017/07/tienda-online.png" class="card-img-top" alt="...">
   <div class="card-body">
-    <h5 class="card-title">Card with stretched link</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <p class="card-text">Categorias</p>
     <a href="categorias" class="btn btn-primary stretched-link">Vamos</a>
   </div>
 </div>   
 <div class="card" style="width: 18rem;">
-  <img src="https://www.ecommercenews.pe/wp-content/uploads/2017/07/tienda-online.png" class="card-img-top" alt="...">
+  <img src="https://blog.comparasoftware.com/wp-content/uploads/2021/02/tipos-de-clientes-de-ecommerce-1.png" class="card-img-top" alt="...">
   <div class="card-body">
-    <h5 class="card-title">Card with stretched link</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="categorias" class="btn btn-primary stretched-link">Vamos</a>
+    <p class="card-text">Clientes</p>
+    <a href="clientes" class="btn btn-primary stretched-link">Vamos</a>
   </div>
-</div><div class="card" style="width: 18rem;">
-  <img src="https://www.ecommercenews.pe/wp-content/uploads/2017/07/tienda-online.png" class="card-img-top" alt="...">
+</div> 
+<div class="card" style="width: 18rem;">
+  <img src="https://eominternacional.com/wp-content/uploads/2019/12/articulos-cientificos.jpg" class="card-img-top" alt="...">
   <div class="card-body">
-    <h5 class="card-title">Card with stretched link</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="categorias" class="btn btn-primary stretched-link">Vamos</a>
+    <p class="card-text">Articulos</p>
+    <a href="articulos" class="btn btn-primary stretched-link">Vamos</a>
   </div>
-</div><div class="card" style="width: 18rem;">
-  <img src="https://www.ecommercenews.pe/wp-content/uploads/2017/07/tienda-online.png" class="card-img-top" alt="...">
+</div> 
+<div class="card" style="width: 18rem;">
+  <img src="https://imborrable.com/wp-content/uploads/2021/01/psicologia-del-color-significados.png" class="card-img-top" alt="...">
   <div class="card-body">
-    <h5 class="card-title">Card with stretched link</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="categorias" class="btn btn-primary stretched-link">Vamos</a>
+    <p class="card-text">Colores</p>
+    <a href="colores" class="btn btn-primary stretched-link">Vamos</a>
   </div>
-</div><div class="card" style="width: 18rem;">
-  <img src="https://www.ecommercenews.pe/wp-content/uploads/2017/07/tienda-online.png" class="card-img-top" alt="...">
+</div> 
+<div class="card" style="width: 18rem;">
+  <img src="https://i.ytimg.com/vi/iwWNW4JWnFw/maxresdefault.jpg" class="card-img-top" alt="...">
   <div class="card-body">
-    <h5 class="card-title">Card with stretched link</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="categorias" class="btn btn-primary stretched-link">Vamos</a>
+    <p class="card-text">Creaciones</p>
+    <a href="creaciones" class="btn btn-primary stretched-link">Vamos</a>
   </div>
-</div>
-<div class="spinner-grow" role="status">
-  <span class="visually-hidden"></span>
-</div>
-
+</div> 
+<div class="card" style="width: 18rem;">
+  <img src="https://encolombia.com/wp-content/uploads/2018/05/Claves-Aumentar-Ventas.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text">Ventas</p>
+    <a href="ventas" class="btn btn-primary stretched-link">Vamos</a>
+  </div>
+</div> 
+<div class="card" style="width: 18rem;">
+  <img src="https://escuelainenka.com/wp-content/uploads/proveedores.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text">Proveedores</p>
+    <a href="proveedores" class="btn btn-primary stretched-link">Vamos</a>
+  </div>
+</div> 
 @stop
 
 @section('css')
